bda7ca75f5fe20eca1b86ee89d968d1952f81c9a
waiga <hyang9387@gmail.com>
2023-01-29 18:25:32 +0900

    images add and API json file uploaded

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+# my blog posts
+POSTS = requests.get("https://api.npoint.io/d66dc8817de42951992f").json()
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    print(POSTS)
+    return render_template("index.html", my_posts=POSTS)
 
 @app.route('/about')
 def about():
