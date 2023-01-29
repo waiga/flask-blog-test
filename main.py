b6f9b8b8b34936c76042d9c6ab23daf3fdb08e1b
waiga <hyang9387@gmail.com>
2023-01-29 16:55:58 +0900

    header and footer fix

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def get_all_posts():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
